0c95db9e55a6912cb6951b769b05fd486d68ffda
Diogoxiang <a233894432@gmail.com>
2018-10-19 13:41:16 +0800

    is练习

---
+++ packages/utils/test/is.test.ts
@@ -0,0 +1,9 @@
+import { isDOMError, isDOMException, isError, isErrorEvent } from "../src/is";
+
+
+describe('isError()', () => {
+    test('should work as advertised', () => {
+        // tslint:disable-next-line:no-console
+        console.log("ss")
+    });
+});
